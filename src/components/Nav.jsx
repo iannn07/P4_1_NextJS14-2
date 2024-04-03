08644f610504c2ead5987bbfbd730fc061dd5849
Iannn <pristianb007@gmail.com>
2024-04-03 19:54:14 +0700

    Fixed Bug CLIENT_FETCH_ERROR

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,134 @@
-import React from 'react'
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+  const { data: session } = useSession();
+  const [providers, setProviders] = useState(null);
+  const [collapse, setCollapse] = useState(false);
 
-export default Nav
+  useEffect(() => {
+    (async () => {
+      const response = await getProviders();
+      setProviders(response);
+    })();
+  }, []);
+
+  return (
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href={'/'} className='flex gap-2 flex-center'>
+        <Image
+          src='assets/images/logo.svg'
+          alt='Logo'
+          width={30}
+          height={30}
+          className='object-contain'
+        />
+        <p className='logo_text'>Gausspy</p>
+      </Link>
+
+      {/* Desktop View */}
+      <div className='sm:flex hidden'>
+        {session?.user ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href={'/create-prompt'} className='black_btn'>
+              Create Post
+            </Link>
+            <button type='button' onClick={signOut} className='outline_btn'>
+              Sign Out
+            </button>
+            <Link href={'/profile'}>
+              <Image
+                src={session?.user.image}
+                alt='Profile'
+                width={37}
+                height={37}
+                className='rounded-full'
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='black_btn'
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile View */}
+      <div className='sm:hidden flex relative'>
+        {session?.user ? (
+          <div className='flex'>
+            <Image
+              src={session?.user.image}
+              alt='Profile'
+              width={37}
+              height={37}
+              className='rounded-full'
+              onClick={() => {
+                setCollapse((prev) => !prev);
+              }}
+            />
+
+            {collapse && (
+              <div className='dropdown'>
+                <Link
+                  href={'/profile'}
+                  className='dropdown_link'
+                  onClick={() => setCollapse(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href={'/create-prompt'}
+                  className='dropdown_link'
+                  onClick={() => setCollapse(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type='button'
+                  onClick={() => {
+                    setCollapse(false);
+                    signOut();
+                  }}
+                  className='mt-5 w-full black_btn'
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='black_btn'
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
