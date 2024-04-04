70a643c81435f0969378b3e00a33c9b591834aec
Iannn <pristianb007@gmail.com>
2024-04-04 19:28:28 +0700

    Fixed Display Name

--- src/components/PromptCard.jsx
+++ src/components/PromptCard.jsx
@@ -45,7 +45,7 @@ const PromptCard = ({
 
           <div className='flex flex-col'>
             <h3 className='font-satoshi font-semibold text-gray-900'>
-              {post.creator.username}
+              {post.creator.name}
             </h3>
             <p className='font-inter text-sm text-gray-500'>
               {post.creator.email}
