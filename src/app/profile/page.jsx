a1535d6b74dd9464187488783935a94ef0c5f0e0
Iannn <pristianb007@gmail.com>
2024-04-04 21:45:43 +0700

    Caching Fix

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -12,7 +12,9 @@ const ProfilePage = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const response = await fetch(`/api/users/${session?.user.id}/posts`, {
+        cache: 'no-store',
+      });
       const data = await response.json();
 
       setPosts(data);
